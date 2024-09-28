4981aa45379b7cfd5cf85ce5dbaf10fad7c442d0
Takudzwa Shumbamhini <takudzwa@cognitivesystems.ai>
2024-09-28 13:00:23 +0200

    migrations and static files setup

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static  # Import static setting you just added
+from mysite.views import *
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', include("mysite.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Add the part after the plus sign (+)
