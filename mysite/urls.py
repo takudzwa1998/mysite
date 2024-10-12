73cf41555d7190fedaf428b4857fbf5e528aaf5d
Takudzwa Shumbamhini <takudzwa@cognitivesystems.ai>
2024-10-12 14:38:38 +0200

    html breakdowns

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="index"),
     path("home/", views.home, name="home"),
-    path("home/about/", views.about, name="about"),
-    path("home/projects/", views.projects, name="projects"),
-    path("home/consulting/", views.consulting, name="consulting"),
-    path("home/blog/", views.blog, name="blog"),
+    path("about/", views.about, name="about"),
+    path("projects/", views.projects, name="projects"),
+    path("consulting/", views.consulting, name="consulting"),
+    path("blog/", views.blog, name="blog"),
 ]
